718afbf856bbccee8efd1c3c3658fd7262216762
root <nagaraja.hm.0001@gmail.com>
2022-01-17 08:38:48 +0100

    Update: submodule Eep

--- LATEST/Eep.cpp
+++ LATEST/Eep.cpp
@@ -5,3 +5,36 @@ class_Eep Eep;
 FUNC(void, EEP_CODE) class_Eep::InitFunction(void){
 }
 
+FUNC(void, EEP_CODE) class_Eep::InitFunction(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::SetMode(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::Read(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::Write(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::Erase(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::Compare(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::Cancel(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::GetStatus(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::GetJobResult(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::GetVersionInfo(void){
+}
+
+FUNC(void, EEP_CODE) class_Eep::MainFunction(void){
+}
+
--- LATEST/Eep.h
+++ LATEST/Eep.h
@@ -5,7 +5,17 @@
 
 class class_Eep{
    public:
-      FUNC(void, EEP_CODE) InitFunction(void);
+      FUNC(void, EEP_CODE) InitFunction   (void);
+      FUNC(void, EEP_CODE) SetMode        (void);
+      FUNC(void, EEP_CODE) Read           (void);
+      FUNC(void, EEP_CODE) Write          (void);
+      FUNC(void, EEP_CODE) Erase          (void);
+      FUNC(void, EEP_CODE) Compare        (void);
+      FUNC(void, EEP_CODE) Cancel         (void);
+      FUNC(void, EEP_CODE) GetStatus      (void);
+      FUNC(void, EEP_CODE) GetJobResult   (void);
+      FUNC(void, EEP_CODE) GetVersionInfo (void);
+      FUNC(void, EEP_CODE) MainFunction   (void);
 };
 
 extern class_Eep Eep;
