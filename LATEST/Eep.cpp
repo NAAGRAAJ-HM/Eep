7b59517c068c265692ca98f1e41eff4ac537440c
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:33 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Eep

--- LATEST/Eep.cpp
+++ LATEST/Eep.cpp
@@ -40,6 +40,16 @@ class module_Eep:
       FUNC(void, EEP_CODE) DeInitFunction (void);
       FUNC(void, EEP_CODE) GetVersionInfo (void);
       FUNC(void, EEP_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, EEP_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
