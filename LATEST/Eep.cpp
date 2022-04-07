286803cd866abcb5e281c6a6c48222dd0c22151c
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:50:09 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Eep

--- LATEST/Eep.cpp
+++ LATEST/Eep.cpp
@@ -96,8 +96,14 @@ FUNC(void, EEP_CODE) module_Eep::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_Eep as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == Eep_InitCheck)
