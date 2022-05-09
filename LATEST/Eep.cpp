e30498e1cb9c8a98c882ffb9e88e2d26218ad1f7
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:20 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Eep

--- LATEST/Eep.cpp
+++ LATEST/Eep.cpp
@@ -35,6 +35,13 @@ class module_Eep:
       public abstract_module
    ,  public class_Eep_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Eep(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
