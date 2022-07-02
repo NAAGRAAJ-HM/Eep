06b946cc90fbf193272107da0e5e96b4ed848f09
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-02 11:40:40 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module for EEPROM

--- LATEST/Eep.cpp
+++ LATEST/Eep.cpp
@@ -82,12 +82,7 @@ FUNC(void, EEP_CODE) module_Eep::InitFunction(
    ){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_HIGH){
-            lptrCfg = lptrCfgModule;
-         }
-         else{
-            lptrCfg = &PBcfgEep;
-         }
+         lptrCfg = lptrCfgModule;
       }
       else{
 #if(STD_ON == Eep_DevErrorDetect)
