f552f9fb50ac4b37b252360f63539650f91d07a6
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-12-27 20:42:16 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for EEPROM

---
+++ InfineonTLE9879QXA40_R2023/McalEep_core.hpp
@@ -0,0 +1,66 @@
+#pragma once
+/******************************************************************************/
+/* File   : McalEep_core.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_McalEep.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCALEEP_CORE_FUNCTIONALITIES                                               \
+              FUNC(void, MCALEEP_CODE) SetMode        (void);                      \
+              FUNC(void, MCALEEP_CODE) Read           (void);                      \
+              FUNC(void, MCALEEP_CODE) Write          (void);                      \
+              FUNC(void, MCALEEP_CODE) Erase          (void);                      \
+              FUNC(void, MCALEEP_CODE) ServiceCompare        (void);                      \
+              FUNC(void, MCALEEP_CODE) McalCancel         (void);                      \
+              FUNC(void, MCALEEP_CODE) GetStatus      (void);                      \
+              FUNC(void, MCALEEP_CODE) GetJobResult   (void);                      \
+
+#define MCALEEP_CORE_FUNCTIONALITIES_VIRTUAL                                       \
+      virtual FUNC(void, MCALEEP_CODE) SetMode        (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) Read           (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) Write          (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) Erase          (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) ServiceCompare        (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) McalCancel         (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) GetStatus      (void) = 0;                  \
+      virtual FUNC(void, MCALEEP_CODE) GetJobResult   (void) = 0;                  \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_McalEep_Functionality{
+   public:
+      MCALEEP_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
